53babe911054227c5a7d22a9ff83db57deefc8d3
Michael Quintela <Michael.QUintela@acuitybrands.com>
2022-07-20 14:10:32 -0400

    Creating shell

---
+++ modern/IAMCandidateModern/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace IAMCandidateModern.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
